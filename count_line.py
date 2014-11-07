8a34cb388028e66fd52cf1091a63c7c593717b46
liqing <liqing.qiya@gmail.com>
2014-11-07 23:33:19 +0800

    统计c/h文件的有效代码行数

---
+++ count_line.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import sys
+import fileinput
+import os
+import fnmatch
+import pdb
+
+
+def count_line(filepath):
+    """
+    computer the line
+    ====================
+    ++i, if the line is not blank.
+    """
+    count = 0
+    file_input = fileinput.input(filepath)
+    for i in file_input:
+        #current_line_no = file_input.filelineno()
+        if i!="\n":
+            count += 1
+    return count
+
+
+def all_file(root=".", pattern="*.[hc]"):
+    """
+    统计指定目录下所有文件的代码行数
+    """
+    line_count = 0
+    file_count = 0
+    for path, subdir, files in os.walk(root):
+        for file_name in files:
+            if fnmatch.fnmatch(file_name, pattern):
+                file_full_name = os.path.join(path, file_name)
+
+                line_count += count_line(file_full_name)
+                file_count += 1
+
+    return file_count, line_count
+
+
+if __name__ == "__main__":
+    print all_file()
